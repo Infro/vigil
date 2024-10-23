10ba634862a2253d6aeee4051e781111f840f403
John B Heilman IV <1735575+Infro@users.noreply.github.com>
2024-10-23 10:48:44 -0700

    Fixed cfg

--- sms_email.py
+++ sms_email.py
@@ -1,18 +1,17 @@
-from cfg import *
 import smtplib
 from email.message import EmailMessage
 from gsecrets import *
 
 
-smtp_server=""
-smtp_ssl=True
-smtp_port=""
-smtp_username=""
-smtp_password=""
+# smtp_server=""
+# smtp_ssl=True
+# smtp_port=""
+# smtp_username=""
+# smtp_password=""
 
 def loginSendQuit(server, to_email, msg):
     # server.esmtp_features['auth'] = 'LOGIN PLAIN'
-    server.login(smtp_from, smtp_password)
+    server.login(smtp_username, smtp_password)
     server.sendmail(smtp_username, to_email, msg.as_string())
     server.quit()
 
@@ -23,20 +22,22 @@ def send_email(to_email, subject, message, override=False):
         msg["To"] = to_email
         msg["Subject"] = subject
 
-        body = (
-            f" {message.author.global_name}:.\n"
-            f"Jump to message {message.jump_url}\n"
-            f"{message.content}"
-        )
+        body = message
+        # (
+        #     f" {message.author.global_name}:.\n"
+        #     f"Jump to message {message.jump_url}\n"
+        #     f"{message.content}"
+        # )
         msg.add_header("Content-Type", "text/plain")
         msg.set_payload(body)
         if smtp_ssl ^ override:
             with smtplib.SMTP(smtp_server, smtp_port) as server:
-                # server.set_debuglevel(True)
+                #server.set_debuglevel(True)
                 server.starttls()
                 self.loginSendQuit(server, to_email, msg)
         else:
             with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+                #server.set_debuglevel(True)
                 loginSendQuit(server, to_email, msg)
 
         print(f"Email sent successfully to {to_email}")
